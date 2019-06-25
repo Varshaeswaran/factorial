ff1d4ef0576261ca3829b3d7d6c5e1d30a151a2a
Varshasarvesh2001 <52190393+Varshasarvesh2001@users.noreply.github.com>
2019-06-25 11:26:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    if(a%2==0)
+         printf("the given number is even");
+    else
+         printf("the given number is odd");
+    return 0;
+}
